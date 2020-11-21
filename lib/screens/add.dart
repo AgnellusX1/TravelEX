552eb91c40dac8966cce010e43912f199342dc52
Sancia Dcunha <sanciadcunha15@gmail.com>
2020-11-21 12:25:08 +0530

    ADDED:App bar, title text view, updated pubspec.yaml

--- lib/screens/add.dart
+++ lib/screens/add.dart
@@ -7,107 +7,56 @@ class Add extends StatefulWidget {
 }
 
 class _AddState extends State<Add> {
-  final _controller = TextEditingController();
+  final _controllertitle=TextEditingController();
+  final _controllerdesc = TextEditingController();
   String name = "";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-
-          actions:<Widget> [
-            IconButton(
-              icon: Icon(Icons.search,
-                color: Colors.white,),
-            )
-          ],
           title: Text("Add A New Memory"),
         ),
         body: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-          Text(name),
-          Container(
-            child: TextField(
-                controller: _controller,
-                decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.transparent),
-                        borderRadius: BorderRadius.all(Radius.circular(40))),
+              Text(name),
+              Container(
+                child: TextField(
+                    controller: _controllertitle,
+                    decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.transparent),
+                            borderRadius: BorderRadius.all(Radius.circular(40))),
                         focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.transparent),
-                        borderRadius: BorderRadius.all(Radius.circular(40))),
-                    hintText: "Enter your Memories",
-                    filled: true,
-                    fillColor: Colors.grey[200])),
-            padding: EdgeInsets.all(32),
-          ),
-          Container(
-            width: double.infinity,
-            child: FlatButton(
-              child: Text("Submit"),
-              color: Colors.red,
-              onPressed: () {
-                // setState((){
-                //   name= _controller.text;
-                // });
-              },
-            ),
-            padding: EdgeInsets.all(32),
-          ),
-        ]));
-        Container(
-                child:TextField(
-                    controller:_controller,
-                    decoration:InputDecoration(
-                        enabledBorder:OutlineInputBorder(
-                            borderSide:BorderSide(color:Colors.transparent),
-                            borderRadius:BorderRadius.all(Radius.circular(40))
-
-                        ),
-                        focusedBorder:OutlineInputBorder(
-                            borderSide:BorderSide(color:Colors.transparent),
-                            borderRadius:BorderRadius.all(Radius.circular(40))
-
-                        ),
-                        hintText:"Title",
-                        filled:true,
-                        fillColor: Colors.grey[200]
-
-                    )
-                ),
+                            borderSide: BorderSide(color: Colors.transparent),
+                            borderRadius: BorderRadius.all(Radius.circular(40))),
+                        hintText: "Title",
+                        filled: true,
+                        fillColor: Colors.grey[200])),
                 padding: EdgeInsets.all(32),
               ),
-
               Container(
-                child:TextField(
-                  controller:_controller,
-                  decoration:InputDecoration(
-                    enabledBorder:OutlineInputBorder(
-                      borderSide:BorderSide(color:Colors.transparent),
-                      borderRadius:BorderRadius.all(Radius.circular(40))
-
-                    ),
-                    focusedBorder:OutlineInputBorder(
-                        borderSide:BorderSide(color:Colors.transparent),
-                        borderRadius:BorderRadius.all(Radius.circular(40))
-
-                    ),
-                    hintText:"Enter your memories",
-                    filled:true,
-                    fillColor: Colors.grey[200]
-
-                  )
-                ),
+                child: TextField(
+                    controller: _controllerdesc,
+                    decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.transparent),
+                            borderRadius: BorderRadius.all(Radius.circular(40))),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.transparent),
+                            borderRadius: BorderRadius.all(Radius.circular(40))),
+                        hintText: "Enter your memories",
+                        filled: true,
+                        fillColor: Colors.grey[200])),
                 padding: EdgeInsets.all(32),
               ),
-
               Container(
-                width:double.infinity,
-                child:FlatButton(
+                width: double.infinity,
+                child: FlatButton(
                   child: Text("Submit"),
-                  color:Colors.red,
-                  onPressed:(){
+                  color: Colors.red,
+                  onPressed: () {
                     // setState((){
                     //   name= _controller.text;
                     // });
@@ -115,12 +64,10 @@ class _AddState extends State<Add> {
                 ),
                 padding: EdgeInsets.all(32),
               ),
-            ]
-        )
-    );
+            ]));
   }
-
 }
+
 
 //TODO
 
