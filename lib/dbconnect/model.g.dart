a0c0ae5dbe66406089ceb72d7d95c28eac8062a9
Agnellus Fernandes <agnellusfernandes@gmail.com>
2020-11-23 11:20:01 +0530

    FIXED: Generated Model

--- lib/dbconnect/model.g.dart
+++ lib/dbconnect/model.g.dart
@@ -21,21 +21,24 @@ class ModelAdapter extends TypeAdapter<Model> {
       fields[1] as String,
       fields[2] as String,
       fields[3] as String,
+      fields[4] as String,
     );
   }
 
   @override
   void write(BinaryWriter writer, Model obj) {
     writer
-      ..writeByte(4)
+      ..writeByte(5)
       ..writeByte(0)
-      ..write(obj.id)
+      ..write(obj.title)
       ..writeByte(1)
       ..write(obj.dateTime)
       ..writeByte(2)
-      ..write(obj.title)
+      ..write(obj.location)
       ..writeByte(3)
-      ..write(obj.location);
+      ..write(obj.image)
+      ..writeByte(4)
+      ..write(obj.description);
   }
 
   @override
